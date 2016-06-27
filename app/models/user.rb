41540d46ed3c044014957437a03f7444d8c7884b
Ahmed Raza <ahmed.raza@square63.com>
2016-06-27 16:38:08 +0500

    Gender and Name fields on edit and registration accounts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
 
+  validates :first_name, presence: true
+
+  GENDER = ["Male", "Female"]
+
   def full_name
     [self.first_name, self.last_name].join(' ')
   end
