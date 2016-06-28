a3b1f6d53b556971d83dc94c4e4143e925326886
Ahmed Raza <ahmed.raza@square63.com>
2016-06-28 13:19:34 +0500

    Profile picture upload functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
 
   protected
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
-      devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :gender])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :gender, :birth_date])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :gender, :birth_date, attachment_attributes: [:id, :image, :_destroy]])
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,26 @@ class User < ActiveRecord::Base
   validates :first_name, presence: true
 
   has_one :attachment, as: :attachable, dependent: :destroy
+  accepts_nested_attributes_for :attachment
 
   GENDER = ["Male", "Female"]
 
   def full_name
     [self.first_name, self.last_name].join(' ')
+  end
+
+  def profile_picture(style)
+    if self.attachment
+      self.attachment.image.url(style)
+    else
+      "/assets/medium/missing.png"
+    end
+  end
+
+  def age
+    if self.birth_date
+      age = (Date.today - self.birth_date).to_i / 365.year
+    "#{age} years"
+    end
   end
 end
